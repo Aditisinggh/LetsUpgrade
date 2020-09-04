781feccd937d7ceaf14c53d206974ccd86660ebd
Pratyush-PS <60107386+Pratyush-PS@users.noreply.github.com>
2020-09-04 15:10:36 +0530

    Create program1.py

---
+++ PythonEssentials/day2/program1.py
@@ -0,0 +1,22 @@
+#LIST and its default functions
+
+mylist = [8,2,9,1,5]
+
+#adding one element at the end of list
+mylist.append(6)
+print("After adding one element:",mylist)
+
+#Sorting myList
+mylist.sort()
+print("Sorted:", mylist)
+
+#Reversing myList
+mylist.reverse()
+print("Reverse:",mylist)
+
+#Adding a list to myList
+mylist.extend([25,1,27])
+print("Extended list:",mylist)
+
+#Returning count of an element in list
+print("1 occurs following times:",mylist.count(1))
